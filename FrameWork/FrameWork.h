59acf0f65b4b3743f09616447e809f5669e5b3e7
Deepak <deepak@deepakmac.local>
2017-05-19 11:33:43 +0530

    Initial commit

---
+++ FrameWork/FrameWork.h
@@ -0,0 +1,19 @@
+//
+//  FrameWork.h
+//  FrameWork
+//
+//  Created by Deepak on 19/05/2016 A.
+//  Copyright © 2017 Adyog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameWork.
+FOUNDATION_EXPORT double FrameWorkVersionNumber;
+
+//! Project version string for FrameWork.
+FOUNDATION_EXPORT const unsigned char FrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWork/PublicHeader.h>
+
+
---
+++ ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FrameWork
+//
+//  Created by Deepak on 19/05/2016 A.
+//  Copyright © 2017 Adyog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+     override func viewDidLoad() {
+        super.viewDidLoad()
+            print("demo")
+    }
+    
+}
+
